499aec9a54b63e94c17f88a098250451a2d7c48b
TAGATSING Samuel <samuelftagat@gmail.com>
2025-10-20 12:09:50 +0100

    Ajustement de la connexion au backend

--- next.config.js
+++ next.config.js
@@ -3,7 +3,12 @@ const createNextIntlPlugin = require('next-intl/plugin');
 const withNextIntl = createNextIntlPlugin();
 
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+
+  // On injecte les variables d'environnement à la build
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080',
+  },
 };
 
 module.exports = withNextIntl(nextConfig);
