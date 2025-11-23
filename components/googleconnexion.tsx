fe196fb76191187c9599d59c75f34e1d729da2be
TAGATSING Samuel <samuelftagat@gmail.com>
2025-11-23 12:44:03 +0100

    Ajustement de la connexion via google OAuth2

--- components/googleconnexion.tsx
+++ components/googleconnexion.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
+import { useTranslations } from 'next-intl';
 
 const loginWithGoogle = () => {
-  window.location.href = "https://farcal-back.onrender.com/login/oauth2/code/google";
+  window.location.href = "https://farcal-back.onrender.com/oauth2/authorization/google";
 };
 
 export default function Googleconnexion() {
+  const t = useTranslations('landing');
   return (
     <button onClick={loginWithGoogle} className='bg-violet-500 text-white px-4 py-2 rounded-md mt-4 hover:bg-violet-600 transition-colors'>
-      Se connecter avec Google
+      {t('google')}
     </button>
   );
 }
