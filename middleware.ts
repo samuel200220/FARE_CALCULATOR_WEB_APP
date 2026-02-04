3f88fadf67610874f13f44d443846f331231f286
NguetchuissiBrunel <landrybrunel5@gmail.com>
2026-02-04 17:42:21 +0100

    rien

--- middleware.ts
+++
@@ -1,9 +0,0 @@
-import createMiddleware from 'next-intl/middleware';
-import { routing } from './i18n/routing';
-
-export default createMiddleware(routing);
-
-export const config = {
-  // Matcher qui exclut les fichiers statiques et API
-  matcher: ['/((?!api|_next|_vercel|.*\\..*).*)']
-};
